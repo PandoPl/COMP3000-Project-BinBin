cc6640c1247d3b9c0c30176c861e2c188af659e2
PandoPl <ppandov0@gmail.com>
2025-12-13 23:38:14 +0000

    App.py
    Initialises the app

---
+++ Program/Backend/app.py
@@ -0,0 +1,26 @@
+# backend/app.py
+from flask import Flask, render_template
+from api import api_bp
+from db import init_db
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///binbin.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Initialise database
+    init_db(app)
+
+    # Register API blueprint
+    app.register_blueprint(api_bp, url_prefix = "/api")
+
+    # Dashboard route
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug = True)
