77e55b122e09b4dc9ee61d76e7f7757131dada16
PandoPl <ppandov0@gmail.com>
2025-12-13 23:39:23 +0000

    Database for storing bins and events
    Creates a database. Bins have a one-to-many relationship with Events. One bin can have many events but an event is associated with a single bin.

---
+++ Program/Backend/db.py
@@ -0,0 +1,10 @@
+# backend/db.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        from models import Bin, Event 
+        db.create_all()
